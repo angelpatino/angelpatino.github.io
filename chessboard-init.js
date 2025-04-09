24ff66363197e1fa07d2ccd47eff20eb8e71babb
angelpatino <theangelpatino@gmail.com>
2025-04-09 04:58:17 -0700

    Chess Board Test

---
+++ chessboard-init.js
@@ -0,0 +1,13 @@
+// chessboard-init.js
+
+$(document).ready(function() {
+  var board = Chessboard('board1', {
+    draggable: true,
+    dropOffBoard: 'trash',
+    sparePieces: true
+  });
+
+  // Set up the initial position for the Polish Opening
+  var polishOpeningPosition = 'rnbqkbnr/pppppppp/8/8/1P6/8/P1PPPPPP/RNBQKBNR w KQkq - 0 1';
+  board.position(polishOpeningPosition);
+});
