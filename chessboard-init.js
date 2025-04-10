fa31bceda352f3531801f52e22cce25994d09674
angelpatino <theangelpatino@gmail.com>
2025-04-10 15:59:38 -0700

    fixed Chessboard.js?

--- chessboard-init.js
+++ chessboard-init.js
@@ -4,10 +4,7 @@ $(document).ready(function() {
   var board = Chessboard('board1', {
     draggable: true,
     dropOffBoard: 'trash',
-    sparePieces: true
+    position: 'rnbqkbnr/pppppppp/8/8/1P6/8/P1PPPPPP/RNBQKBNR w KQkq - 0 1'
   });
 
-  // Set up the initial position for the Polish Opening
-  var polishOpeningPosition = 'rnbqkbnr/pppppppp/8/8/1P6/8/P1PPPPPP/RNBQKBNR w KQkq - 0 1';
-  board.position(polishOpeningPosition);
 });
