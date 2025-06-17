0c985115087306a8ed010e91938431fdb2db2e41
angelpatino <theangelpatino@gmail.com>
2025-06-16 22:46:46 -0700

    Adding images

--- chessboard-init.js
+++ chessboard-init.js
@@ -1,10 +1,6 @@
-// chessboard-init.js
+const config = {
+  pieceTheme: 'img/chesspieces/alpha/{piece}.png',
+  position: 'rnbqkbnr/pppppppp/8/8/1P6/8/P1PPPPPP/RNBQKBNR w KQkq - 0 1'
+}
 
-$(document).ready(function() {
-  var board = Chessboard('board1', {
-    draggable: true,
-    dropOffBoard: 'trash',
-    position: 'rnbqkbnr/pppppppp/8/8/1P6/8/P1PPPPPP/RNBQKBNR w KQkq - 0 1'
-  });
-
-});
+const board = Chessboard2('board1', config)
